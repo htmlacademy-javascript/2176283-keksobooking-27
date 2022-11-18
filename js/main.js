2e262148125a87b5bcf2f5897a74ac00a15cec0b
Ivan-Pelipenko <ivan-pelipenko@inbox.ru>
2022-11-18 18:46:04 +0300

    8.21. Правда или действие (часть 1)

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 import { renderAds } from './popup.js';
 import { generateAds } from './data.js';
 import {enablePage, disablePage} from './page.js';
+import './ad-form.js';
 
-enablePage();
-disablePage();
+//enablePage();
+//disablePage();
 
 const drawAd = generateAds(10);
 renderAds(drawAd[6]);
